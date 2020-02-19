ab10a61abf9b88e8afca5843ccbb7747878088d0
Mike Harley <mharley12345@gmail.com>
2020-02-19 12:23:10 -0800

    social get_all_social_paths

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,11 @@
+import random
+from util import Stack,Queue
+
 class User:
     def __init__(self, name):
         self.name = name
+    def __repr__(self):
+        return self.name
 
 class SocialGraph:
     def __init__(self):
@@ -45,8 +50,31 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i+1}")
 
         # Create friendships
+        # create a list with all possible friendships
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+
+        # Shuffle the list
+        random.shuffle(possible_friendships)
+        print("----")
+        print(possible_friendships)
+        print("----")
+        # Grab the first N pairs from the list and create those friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+
+        # avg_friendships = total_friendships / num_users
+        # total_friendships = avg_friendships * num_users
+        # N = avg_friendships * num_users // 2
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,7 +85,36 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+        
+       
+        
+        
+        q = Queue()
+        q.enqueue( [user_id] )
         visited = {}  # Note that this is a dictionary, not a set
+        # visited.update(test='value')
+        print("visited dict: ", visited)
+
+        while q.size() > 0:
+            path = q.dequeue()
+            user = path[-1]
+            if user not in visited:
+                # print("NOT IN VISITED", user)
+                visited.update({user:path})
+                for friend_id in self.friendships[user_id]:
+                    # for f in friend:
+                        
+
+                    print("=====>", friend_id)
+                    new_path = path.copy()
+
+                    new_path.append(friend_id)
+
+                   # print(q.size())
+                    q.enqueue(new_path)
+                    #print(q.size())
+            
+            
         # !!!! IMPLEMENT ME
         return visited
 
@@ -65,6 +122,9 @@ class SocialGraph:
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
+    print("USERS:")
+    print(sg.users)
+    print("FRIENDSHIPS:")
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
